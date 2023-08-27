134ef87b0cc65aff99890b3b5cd37e62841135e3
Vinícius Lima <vinilcoelho_25@hotmail.com>
2023-08-27 13:31:57 -0300

    Ajustes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -15,4 +15,16 @@
 
     $('#btnPrimary').attr("onclick", "downloadTorrent(" + id +   ")").html("Baixar");
     $('#btnSecondary').html("Cancelar");
+};
+
+var loadingToF = false;
+
+function showHideLoading(id) {
+    loadingToF = !loadingToF;
+    if (loadingToF) {
+        $(id).html("<img src='/img/loading.gif' style='position: relative; left: 50%; top: 50%; width: 50px; padding-top: 20px;  transform: translate(-50%, -50%);' alt='Aguarde...''>");
+    }
+    else {
+        $(id).html("");
+    }
 }
